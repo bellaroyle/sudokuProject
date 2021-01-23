45e32cf1d337046857c7cf69eafba86813f0064c
bella <bella.royle@gmail.com>
2021-01-23 19:27:28 +0000

    refactored so that empty cells are [], removed np arrays and just using list

--- helper-functions/helperFuncs.py
+++ helper-functions/helperFuncs.py
@@ -11,28 +11,51 @@ squareCoordinates = [
         [7, 7, 7, 8, 8, 8, 9, 9, 9],
         [7, 7, 7, 8, 8, 8, 9, 9, 9]
     ];
-sudoku = np.array([
-    [7, 6, 3, 0, 0, 5, 0, 0, 9],
-    [0, 1, 5, 0, 0, 2, 3, 7, 0],
-    [9, 2, 8, 0, 0, 4, 0, 0, 1],
-    [0, 0, 0, 5, 3, 0, 9, 8, 0],
-    [0, 3, 0, 6, 0, 9, 2, 5, 0],
-    [0, 0, 9, 0, 2, 0, 0, 1, 0],
-    [0, 0, 0, 2, 1, 0, 7, 4, 0],
-    [0, 5, 0, 4, 0, 0, 0, 3, 0],
-    [0, 8, 0, 0, 5, 3, 1, 0, 0,],
-])
+sudoku = [
+    [7, 6, 3, [], [], 5, [], [], 9],
+    [[], 1, 5, [], [], 2, 3, 7, []],
+    [9, 2, 8, [], [], 4, [], [], 1],
+    [[], [], [], 5, 3, [], 9, 8, []],
+    [[], 3, [], 6, [], 9, 2, 5, []],
+    [[], [], 9, [], 2, [], [], 1, []],
+    [[], [], [], 2, 1, [], 7, 4, []],
+    [[], 5, [], 4, [], [], [], 3, []],
+    [[], 8, [], [], 5, 3, 1, [], []],
+]
+
+solvedSudoku=[
+    [8, 2, 7, 1, 5, 4, 3, 9, 6],
+    [9, 6, 5, 3, 2, 7, 1, 4, 8],
+    [3, 4, 1, 6, 8, 9, 7, 5, 2],
+    [5, 9, 3, 4, 6, 8, 2, 7, 1],
+    [4, 7, 2, 5, 1, 3, 6, 8, 9],
+    [6, 1, 8, 9, 7, 2, 4, 3, 5],
+    [7, 8, 6, 2, 3, 5, 9, 1, 4],
+    [1, 5, 4, 7, 9, 6, 8, 2, 3],
+    [2, 3, 9, 8, 4, 1, 5, 6, 7]
+]
 
 def getRow(grid,row):
     # function takes a sudoku grid and a row index and returns the values in the row
-    return grid[row]
+    rowContents=[]
+    for i in range (9):
+        if (not grid[row][i] == []):
+            rowContents.append(grid[row][i])
+
+    return rowContents
+
+
 
 def getColumn(grid,column):
         # function takes a sudoku grid and a column index and returns the values in the column
     col=[]
     for i in range (9):
-        col.append(grid[i][column])
+        if (not grid[i][column] == []):
+            col.append(grid[i][column])
     return col
+
+
+
 
 def getSquare(grid,row,col):
         # function takes a sudoku grid and a square index and returns the values in the square
@@ -51,9 +74,13 @@ def getSquare(grid,row,col):
     inSquare=[]
     for r in range(9):
         for c in range(9):
-            if (square == squareCoordinates[r][c]):
+            if (square == squareCoordinates[r][c] and not grid[r][c] == [] ):
                 inSquare.append(grid[r][c])
     return inSquare
+
+
+
+
 
 def isSolved(grid):
         # function takes a sudoku grid and checks all rows, columns and squares to see if it is solved 
@@ -65,29 +92,29 @@ def isSolved(grid):
     squareSolved = []
 
     for i in range(9):
-        rowSolved.append((np.sort(getRow(grid,i)) == allNums).all())
-    if (False in rowSolved):
-        return False
+        if ( not len(getRow(grid,i)) == 9):
+            return False
 
     for j in range(9):
-        colSolved.append((np.sort(getColumn(grid,j)) == allNums).all())
-    if (False in colSolved):
-        return False
+        if ( not len(getColumn(grid,i)) == 9):
+            return False
+
 
     rowsToCheck=[0,3,6]
     colsToCheck = [0,3,6]
     for row in rowsToCheck:
         for col in colsToCheck:
-            squareSolved.append((np.sort(getSquare(grid,row,col)) == allNums).all())
-    if (False in squareSolved):
-        return False
+            if( not len(getSquare(grid,row,col)) == 9):
+                return False
     
     return True
+
+
 
 def fillCell(grid,row,col):
     # finds all possibilities for a given, empty cell, and stores them in a sub array 
     # if only 1 possibility then fills cell
-    rowContents = getRow(grid,row).tolist()
+    rowContents = getRow(grid,row) #.tolist()
     colContents = getColumn(grid,col)
     squareContents = getSquare(grid,row,col)
 
@@ -99,11 +126,13 @@ def fillCell(grid,row,col):
         if (i not in numsUsed):
             possibilities.append(i)
     print(possibilities)
-
     if (len(possibilities) == 1):
         grid[row][col] = possibilities[0]
         return True
     else:
-        grid[row][col] = np.array(possibilities)
+        grid[row][col] = possibilities
+        print(grid[row][col])
+        
         return False 
-    
+print(fillCell(sudoku,0,3))
+print(sudoku)
--- helper-functions/test_helperFuncs.py
+++ helper-functions/test_helperFuncs.py
@@ -2,19 +2,19 @@ import unittest
 import helperFuncs
 import numpy as np 
 
-sudoku = np.array([
-    [7, 6, 3, 0, 0, 5, 0, 0, 9],
-    [0, 1, 5, 0, 0, 2, 3, 7, 0],
-    [9, 2, 8, 0, 0, 4, 0, 0, 1],
-    [0, 0, 0, 5, 3, 0, 9, 8, 0],
-    [0, 3, 0, 6, 0, 9, 2, 5, 0],
-    [0, 0, 9, 0, 2, 0, 0, 1, 0],
-    [0, 0, 0, 2, 1, 0, 7, 4, 0],
-    [0, 5, 0, 4, 0, 0, 0, 3, 0],
-    [0, 8, 0, 0, 5, 3, 1, 0, 0,],
-])
+sudoku = [
+    [7, 6, 3, [], [], 5, [], [], 9],
+    [[], 1, 5, [], [], 2, 3, 7, []],
+    [9, 2, 8, [], [], 4, [], [], 1],
+    [[], [], [], 5, 3, [], 9, 8, []],
+    [[], 3, [], 6, [], 9, 2, 5, []],
+    [[], [], 9, [], 2, [], [], 1, []],
+    [[], [], [], 2, 1, [], 7, 4, []],
+    [[], 5, [], 4, [], [], [], 3, []],
+    [[], 8, [], [], 5, 3, 1, [], [],],
+]
 
-solvedSudoku=np.array([
+solvedSudoku=[
     [8, 2, 7, 1, 5, 4, 3, 9, 6],
     [9, 6, 5, 3, 2, 7, 1, 4, 8],
     [3, 4, 1, 6, 8, 9, 7, 5, 2],
@@ -24,7 +24,7 @@ solvedSudoku=np.array([
     [7, 8, 6, 2, 3, 5, 9, 1, 4],
     [1, 5, 4, 7, 9, 6, 8, 2, 3],
     [2, 3, 9, 8, 4, 1, 5, 6, 7]
-])
+]
 
 
 class TestHelperFuncs(unittest.TestCase):
@@ -32,26 +32,26 @@ class TestHelperFuncs(unittest.TestCase):
     def test_getRow(self):
         # gets correct row
         result1 = helperFuncs.getRow(sudoku,3)
-        np.testing.assert_array_equal(result1,[0, 0, 0, 5, 3, 0, 9, 8, 0])
+        np.testing.assert_array_equal(result1,[ 5, 3, 9, 8])
 
         result2 = helperFuncs.getRow(sudoku,8)
-        np.testing.assert_array_equal(result2,[0, 8, 0, 0, 5, 3, 1, 0, 0,])
+        np.testing.assert_array_equal(result2,[ 8, 5, 3, 1])
     
     def test_getColumn(self):
         # gets correct column
         result1 = helperFuncs.getColumn(sudoku,3)
-        np.testing.assert_array_equal(result1,[0, 0, 0, 5, 6, 0, 2, 4, 0])
+        np.testing.assert_array_equal(result1,[5, 6, 2, 4])
 
         result2 = helperFuncs.getColumn(sudoku,5)
-        np.testing.assert_array_equal(result2,[5, 2, 4, 0, 9, 0, 0, 0, 3])
+        np.testing.assert_array_equal(result2,[5, 2, 4, 9, 3])
 
     def test_getSquare(self):
         # gets correct square
         result1 = helperFuncs.getSquare(sudoku,1,8)
-        np.testing.assert_array_equal(result1,[0, 0, 9, 3, 7, 0, 0, 0, 1])
+        np.testing.assert_array_equal(result1,[9, 3, 7, 1])
 
         result2 = helperFuncs.getSquare(sudoku,7,7)
-        np.testing.assert_array_equal(result2, [7, 4, 0, 0, 3, 0, 1, 0, 0])
+        np.testing.assert_array_equal(result2, [7, 4, 3, 1])
 
     def test_isSolved(self):
         # returns true for a fully solved sudoku
@@ -64,33 +64,33 @@ class TestHelperFuncs(unittest.TestCase):
 
     def test_fillCell(self):
         # returns true and fills cell if only one possibility 
-        testSudoku = np.array([
-            [7, 6, 3, 0, 0, 5, 0, 0, 9],
-            [0, 1, 5, 0, 0, 2, 3, 7, 0],
-            [9, 2, 8, 0, 0, 4, 0, 0, 1],
-            [0, 0, 0, 5, 3, 0, 9, 8, 0],
-            [0, 3, 0, 6, 0, 9, 2, 5, 0],
-            [0, 0, 9, 0, 2, 0, 0, 1, 0],
-            [0, 0, 0, 2, 1, 0, 7, 4, 0],
-            [0, 5, 0, 4, 0, 0, 0, 3, 0],
-            [0, 8, 0, 0, 5, 3, 1, 0, 0,],
-        ])
+        testSudoku = [
+            [7, 6, 3, [], [], 5, [], [], 9],
+            [[], 1, 5, [], [], 2, 3, 7, []],
+            [9, 2, 8, [], [], 4, [], [], 1],
+            [[], [], [], 5, 3, [], 9, 8, []],
+            [[], 3, [], 6, [], 9, 2, 5, []],
+            [[], [], 9, [], 2, [], [], 1, []],
+            [[], [], [], 2, 1, [], 7, 4, []],
+            [[], 5, [], 4, [], [], [], 3, []],
+            [[], 8, [], [], 5, 3, 1, [], [],],
+        ]
         result1 = helperFuncs.fillCell(testSudoku,0,4)
         self.assertEqual(result1, True)
         self.assertEqual(testSudoku[0][4], 8)
 
         # returns false and fills cell with list of possibilities if more than 1 
-        testSudoku = np.array([
-            [7, 6, 3, 0, 0, 5, 0, 0, 9],
-            [0, 1, 5, 0, 0, 2, 3, 7, 0],
-            [9, 2, 8, 0, 0, 4, 0, 0, 1],
-            [0, 0, 0, 5, 3, 0, 9, 8, 0],
-            [0, 3, 0, 6, 0, 9, 2, 5, 0],
-            [0, 0, 9, 0, 2, 0, 0, 1, 0],
-            [0, 0, 0, 2, 1, 0, 7, 4, 0],
-            [0, 5, 0, 4, 0, 0, 0, 3, 0],
-            [0, 8, 0, 0, 5, 3, 1, 0, 0,],
-        ])
+        testSudoku = [
+            [7, 6, 3, [], [], 5, [], [], 9],
+            [[], 1, 5, [], [], 2, 3, 7, []],
+            [9, 2, 8, [], [], 4, [], [], 1],
+            [[], [], [], 5, 3, [], 9, 8, []],
+            [[], 3, [], 6, [], 9, 2, 5, []],
+            [[], [], 9, [], 2, [], [], 1, []],
+            [[], [], [], 2, 1, [], 7, 4, []],
+            [[], 5, [], 4, [], [], [], 3, []],
+            [[], 8, [], [], 5, 3, 1, [], [],],
+        ]
         result2 = helperFuncs.fillCell(testSudoku,0,3)
         self.assertEqual(result2, False)
-        # self.assertEquals(testSudoku[0][4], [])
+        self.assertEqual(testSudoku[0][4], [1,8])
